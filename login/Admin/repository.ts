e6c5e6ace0d13749036838810abe1ed35c52972c
baldhashreya <90861621+baldhashreya@users.noreply.github.com>
2022-02-09 11:38:14 +0530

    Delete login directory

--- login/Admin/repository.ts
+++
@@ -1,9 +0,0 @@
-import  {dbContact} from "../models/index";
-import { ContactUs } from "../models/contactus";
-
-export class ContactUsRepository{
-    
-    public async createContactUs(contactus: ContactUs): Promise<ContactUs> {
-        return dbContact.ContactUs.create(contactus);
-    }
-}
