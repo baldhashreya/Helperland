d048a6cce78f92420cc6df9c25eac5eb5ee7d2c1
baldhashreya <90861621+baldhashreya@users.noreply.github.com>
2022-02-19 14:23:29 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -6,8 +6,7 @@ const app = express();
 import routes from "./router/Rcontactus";
 import User_routes from "./router/User";
 import cookieParser from 'cookie-parser';
-
-
+import book_routes from './router/book';
 dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
@@ -15,17 +14,19 @@ app.use(cookieParser());
 
 app.use('/admin',routes);
 app.use('/',User_routes);
+app.use('/user',book_routes);
+
+
 
 
 app.listen(process.env.PORT, () => {
     console.log(`Server Start at ${process.env.PORT}`)
     sequelize.authenticate().then(async() => {
         console.log("Database Connected");
-
         try {
-            await sequelize.sync()
+            await sequelize.sync({alter : true})
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
 
     }).catch( (e: any) => {
