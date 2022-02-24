d81223fce370b65b7005e7b810de9f96e4a6edff
baldhashreya <90861621+baldhashreya@users.noreply.github.com>
2022-02-24 15:05:55 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
-
-import express from "express";
-import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUI from "swagger-ui-express";
+import express from "express";
+import YAMl from 'yamljs';
+const swaggerJSDoc = YAMl.load('./api.yaml');
 import dotenv from "dotenv";
 import {sequelize} from "./models";
 import routes from "./router/Rcontactus";
@@ -11,35 +11,35 @@ import book_routes from './router/book';
 dotenv.config();
 const app = express();
 
-const swaggerOption={
-    definition:{
-        openapi:'3.0.3',
-        info:{
-            title:'Helperland',
-            version: '1.0.0',
-            description:'Herlperland is a platform where the service providers i.e. cleaners can register themselves for providing services through the portal and would receive the services booked by the customers. The other type of users Customers can book the service requests for cleaning and get the job done by one of the service providers from the portal',
-            contact:{
-                email:'baldha.shreya027@gamil.com',
-                name:"Shreya Baldha"
-            },
-            servers:[
-                {
-                url: "http://localhost:7000"
-                }
-            ]
-        }
-    },
-    apis:["./router/Rcontactus.ts"]
-}
-const swaggerDocs = swaggerJSDoc(swaggerOption);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+// const swaggerOption={
+//     definition:{
+//         openapi:'3.0.3',
+//         info:{
+//             title:'Helperland',
+//             version: '1.0.0',
+//             description:'Herlperland is a platform where the service providers i.e. cleaners can register themselves for providing services through the portal and would receive the services booked by the customers. The other type of users Customers can book the service requests for cleaning and get the job done by one of the service providers from the portal',
+//             contact:{
+//                 email:'baldha.shreya027@gamil.com',
+//                 name:"Shreya Baldha"
+//             },
+//             servers:[
+//                 {
+//                 url: "http://localhost:7000"
+//                 }
+//             ]
+//         },
+//     },
+//     apis:["./router/Rcontactus.ts","./router/User.ts","./router/book.ts"]
+// }
+// const swaggerDocs = swaggerJSDoc(swaggerOption);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJSDoc));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use(cookieParser());
 
 app.use('/tarinee2021/contact',routes);
-app.use('/',User_routes);
+app.use('/trainee2021',User_routes);
 app.use('/user',book_routes);
 
 
